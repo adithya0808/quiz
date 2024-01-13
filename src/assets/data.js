b157e36b84754c7184cf06058793260e92442d7a
My Name <myemail@example.com>
2024-01-13 15:51:25 +0530

    initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,39 @@
+const data=[
+    {
+        question:"Which is the national animal of india?",
+        option1:"Tiger",
+        option2:"Lion",
+        option3:"Leopard",
+        option4:"Elephant",
+        ans:1
+    },{
+        question:"Which is the national animal of pakistan?",
+        option1:"Tiger",
+        option2:"Lion",
+        option3:"Markhor",
+        option4:"Elephant",
+        ans:3
+    },{
+        question:"Which is the national animal of america?",
+        option1:"Tiger",
+        option2:"Lion",
+        option3:"Markhor",
+        option4:"American Bison",
+        ans:4
+    },{
+        question:"Which is the national animal of japan?",
+        option1:"Tiger",
+        option2:"Lion",
+        option3:"Macaque",
+        option4:"Elephant",
+        ans:3
+    },{
+        question:"Which is the national animal of nepal?",
+        option1:"Tiger",
+        option2:"Lion",
+        option3:"Cow",
+        option4:"Elephant",
+        ans:3
+    }
+]
+export default data
